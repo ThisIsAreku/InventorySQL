2363f8fa782086c67763c2c3ad558734b84ad39b
ThisisAreku <this.is.areku@hotmail.fr>
2012-12-24 00:07:29 +0100

    Adding pending part

--- web/api.php
+++ web/api.php
@@ -29,4 +29,115 @@ switch($_GET['p'])
 		}
 		echo json_encode(array('success' => true, 'users' => $u));
 	break;
+	case 'inv':
+		$q = $dbh->prepare('SELECT
+			`inventory`.`id` AS `p_id`,
+			`inventory`.`item` AS `item`,
+			`inventory`.`data` AS `data`,
+			`inventory`.`damage` AS `damage`,
+			`inventory`.`count` AS `count`,
+			`inventory`.`date` AS `date`,
+			`inventory`.`slot` AS `slot`,
+			`enchantments`.`ench` AS `ench`,
+			`enchantments`.`level` AS `level`,
+			`meta`.`key` AS `meta_key`,
+			`meta`.`value` AS `meta_value`
+			FROM `'.$invsql_tableprefix.'_inventories` as `inventory`
+			LEFT JOIN `'.$invsql_tableprefix.'_enchantments` AS `enchantments` ON `inventory`.`id` = `enchantments`.`id` AND `enchantments`.`is_backup` = 0
+			LEFT JOIN `'.$invsql_tableprefix.'_meta` AS `meta` ON `inventory`.`id` = `meta`.`id` AND `meta`.`is_backup` = 0
+			WHERE (`inventory`.`owner` = ? AND `inventory`.`world` = ?) ORDER BY `slot` ASC');
+		$q->execute(array($_GET['u'], $_GET['w']));
+		$inv = array();
+		while($row = $q->fetch())
+		{
+			$s = $row['p_id'];
+			if(!isset($inv[$s]))
+			{
+				$inv[$s] = array();
+			}
+			$inv[$s]['item'] = $row['item'];
+			if(isset($items[$row['item']]))
+			{
+				$inv[$s]['item_name'] = $items[$row['item']];
+			}else{
+				$inv[$s]['item_name'] = "Unknown name";
+			}
+			$inv[$s]['slot'] = $row['slot'];
+			$inv[$s]['data'] = $row['data'];
+			$inv[$s]['damage'] = $row['damage'];
+			$inv[$s]['count'] = $row['count'];
+			$inv[$s]['date'] = $row['date'];
+			
+			if(isset($row['ench']))
+			{
+				if(!isset($inv[$s]['ench']))
+				{
+					$inv[$s]['ench'] = array();
+				}
+				$inv[$s]['ench'][] = array('id' => $row['ench'], 'level' => $row['level']);
+			}
+			if(isset($row['meta_key']))
+			{
+				if(!isset($inv[$s]['meta']))
+				{
+					$inv[$s]['meta'] = array();
+				}
+				$inv[$s]['meta'][] = array('key' => $row['meta_key'], 'value' => $row['meta_value']);
+			}
+		}
+		echo json_encode(array('success' => true, 'inv' => $inv));		
+	break;
+	case 'pendings':
+		$q = $dbh->prepare('SELECT
+			`pendings`.`id` AS `p_id`,
+			`pendings`.`item` AS `item`,
+			`pendings`.`data` AS `data`,
+			`pendings`.`damage` AS `damage`,
+			`pendings`.`count` AS `count`,
+			`enchantments`.`ench` AS `ench`,
+			`enchantments`.`level` AS `level`,
+			`meta`.`key` AS `meta_key`,
+			`meta`.`value` AS `meta_value`
+			FROM `'.$invsql_tableprefix.'_pendings` as `pendings`
+			LEFT JOIN `'.$invsql_tableprefix.'_enchantments` AS `enchantments` ON `pendings`.`id` = `enchantments`.`id` AND `enchantments`.`is_backup` = 0
+			LEFT JOIN `'.$invsql_tableprefix.'_meta` AS `meta` ON `pendings`.`id` = `meta`.`id` AND `meta`.`is_backup` = 0
+			WHERE (`pendings`.`owner` = ? AND `pendings`.`world` = ?)');
+		$q->execute(array($_GET['u'], $_GET['w']));
+		$inv = array();
+		while($row = $q->fetch())
+		{
+			$s = $row['p_id'];
+			if(!isset($inv[$s]))
+			{
+				$inv[$s] = array();
+			}
+			$inv[$s]['item'] = $row['item'];
+			if(isset($items[$row['item']]))
+			{
+				$inv[$s]['item_name'] = $items[$row['item']];
+			}else{
+				$inv[$s]['item_name'] = "Unknown name";
+			}
+			$inv[$s]['data'] = $row['data'];
+			$inv[$s]['damage'] = $row['damage'];
+			$inv[$s]['count'] = $row['count'];
+			
+			if(isset($row['ench']))
+			{
+				if(!isset($inv[$s]['ench']))
+				{
+					$inv[$s]['ench'] = array();
+				}
+				$inv[$s]['ench'][] = array('id' => $row['ench'], 'level' => $row['level']);
+			}
+			if(isset($row['meta_key']))
+			{
+				if(!isset($inv[$s]['meta']))
+				{
+					$inv[$s]['meta'] = array();
+				}
+				$inv[$s]['meta'][] = array('key' => $row['meta_key'], 'value' => $row['meta_value']);
+			}
+		}
+		echo json_encode(array('success' => true, 'pendings' => $inv));		
 }
--- web/js/scripts.js
+++ web/js/scripts.js
@@ -12,9 +12,73 @@ $(function(){
 		$('#users-sidelist .first-header').text('Users');
 	});
 	$('#users-sidelist').on("click", "li.user-selector a", function(event){
+		$('#users-sidelist li.user-selector.active').removeClass('active');
+		$(this).parent().addClass('active');
 		current_userid = $(this).data('userid');
 		current_username = $(this).data('username');
 		$('#user-summary h1').text(current_username);
 		$('#user-summary p').text("Loading data for " + current_username + "...");
+		load_inv();
+		load_pend();
+		$('#user-data').addClass('in');
+	});
+	$('#inventory-table').on("click", "td.clickable", function(event){
+		$('#slot-detail').parent().addClass('in');
+		$('#slot-detail .name').text($(this).data('name'));
+		$('#slot-detail .id').text($(this).data('id'));
+		$('#slot-detail .data').text($(this).data('data'));
+		$('#slot-detail .damage').text($(this).data('damage'));
+		$('#slot-detail .count').text($(this).data('count'));
+	});
+	$('#pendings-table').on("click", "a.details", function(event){
+		event.preventDefault();
+	});
+	$('#reload-inventory').click(function(event){
+		event.preventDefault();
+		$(this).children('i').addClass('ani rotate');
+		setTimeout(function(){
+			$('#reload-inventory i').removeClass('ani rotate');
+		}, 2000);
+		load_inv();
+	});
+	$('#reload-pendings').click(function(event){
+		event.preventDefault();
+		$(this).children('i').addClass('ani rotate');
+		setTimeout(function(){
+			$('#reload-pendings i').removeClass('ani rotate');
+		}, 2000);
+		load_pend();
 	});
 });
+function load_inv()
+{
+	$('#inventory-table td').removeClass('clickable').text('');
+	$.getJSON('./api.php',{'p': 'inv','u': current_userid,'w': 'world'}, function(data){
+		$.each(data.inv, function(index, value) {
+			$el = $('#inventory-table .slot-'+value.slot);
+			$el.addClass('clickable').text(value.item);
+			$el.data('slotid', value.slot); //trick to not have to type every data attribute in html..
+			$el.data('name', value.item_name);
+			$el.data('data', value.data);
+			$el.data('damage', value.damage);
+			$el.data('count', value.count);
+			$el.data('id', value.item);
+			
+		});
+	});
+}
+function load_pend()
+{
+	$('#pendings-table tbody').empty();
+	$.getJSON('./api.php',{'p': 'pendings','u': current_userid,'w': 'world'}, function(data){
+		$.each(data.pendings, function(index, value) {
+			$el = $('#pendings-table tbody').append('<tr><td>'+value.item_name+'</td><td>'+value.item+'</td><td>'+value.data+'</td><td>'+value.damage+'</td><td>'+value.count+'</td><td><a href="#" class="details">details</a></td></tr>');
+			$el.data('name', value.item_name);
+			$el.data('data', value.data);
+			$el.data('damage', value.damage);
+			$el.data('count', value.count);
+			$el.data('id', value.item);
+			
+		});
+	});
+}
